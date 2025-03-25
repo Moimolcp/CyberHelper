2ce4ae10281be33adb88780cfbd3f01cf5a52f6e
Luis Castro <moimolcp@gmail.com>
2025-03-25 00:46:30 -0500

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    target: "esnext"
+  },
   server: {
     headers: {
       'Accept-Ranges': 'bytes',
